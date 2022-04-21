1a341fe3f6d07292c8b483164f5bf9ad267b3139
fajrinurf <fajrinurfauzan@student.telkomuniversity.ac.id>
2022-04-21 22:52:48 +0700

    Add Solidity File

---
+++ Week 02/Hello World v2.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+// Fajri Nurfauzan || 1103180184
+
+pragma solidity ^0.8.1;
+
+contract MyContract {
+    string public myString = 'hello world';
+}
---
+++ Week 02/Hello World.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+// Fajri Nurfauzan || 1103180184
+
+pragma solidity ^0.8.7;
+
+contract HelloWorld {
+    function sayHello() public pure returns (string memory) {
+        return "Hello World!";
+    }
+}
